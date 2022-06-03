5fe399faa6137a0234e74c412ad22d873fd0ca7a
Vladimir Kritov <v.kritov@smartweb.com.ua>
2022-06-03 16:48:32 +0300

    fixed_locators

--- olx.py
+++ olx.py
@@ -33,81 +33,96 @@ def write_data(href, chat_id):
 def find_in_olx(requests_url, chat_id):
     logging.info(f'[olx.ua] Пошук оголошень: {requests_url}')
 
-    res = requests.get(requests_url)
-    soup = bs4.BeautifulSoup(res.text, features="html.parser")
-    if soup.select(EMPTY_LOCATOR):
-        logging.info('[olx.ua] Нічого не знайдено!')
-    elif soup.select(FOUND_LOCATOR):
-        hrefs = [
-            link.get('href') for link in soup.select(FOUND_LOCATOR)
-        ]
-        images = [
-            img.get('src').split(';')[0] for img in soup.select(IMAGE_LOCATOR)
-        ]
-        titles = [
-            title.string for title in soup.select(TITLE_LOCATOR)
-        ]
-        prices = [
-            price.string for price in soup.select(PRICE_LOCATOR)
-        ]
-        locations = [
-            str(location)
-            .replace('<span><i data-icon="location-filled"></i>', '')
-            .replace('</span>', '')
-            for location in soup.select(LOCATION_LOCATOR)
-            if 'location-filled' in str(location)
-        ]
+    olx_work = False
+    for _ in range(5):
 
-        old_hrefs = read_data(chat_id=chat_id)
-        hrefs_and_images = zip(hrefs, images, titles, prices, locations)
+        time.sleep(1)
 
-        ad_found = False
+        res = requests.get(requests_url)
+        soup = bs4.BeautifulSoup(res.text, features="html.parser")
+        if ' 0 ' in soup.select(EMPTY_LOCATOR)[0]:
+            logging.info('[olx.ua] Нічого не знайдено!')
+            olx_work = True
+            break
+        elif soup.select(FOUND_LOCATOR):
+            hrefs = [
+                link.get('href') for link in soup.select(FOUND_LOCATOR)
+            ]
+            images = [
+                img.get('src').split(';')[0] for img in
+                soup.select(IMAGE_LOCATOR)
+            ]
+            titles = [
+                title.string for title in soup.select(TITLE_LOCATOR)
+            ]
+            prices = [
+                price.contents[0] for price in soup.select(PRICE_LOCATOR)
+            ]
+            locations = [
+                location.contents[0] for location in
+                soup.select(LOCATION_LOCATOR)
+            ]
 
-        for href_image in hrefs_and_images:
-            href, image, title, price, location = href_image
-            format_href = href.split('#')[0]
+            old_hrefs = read_data(chat_id=chat_id)
+            hrefs_and_images = zip(hrefs, images, titles, prices, locations)
 
-            if format_href not in old_hrefs:
-                logging.info(
-                    f'[olx.ua] Знайдено нове оголошення: {format_href}'
-                )
-                ad_found = True
-                stop = False
-                sent_status = 'false'
-                for stop_word in STOP_WORDS:
-                    if stop_word.lower() in title.lower():
-                        stop = stop_word
-                        break
+            ad_found = False
 
-                if not stop:
-                    sent_status = send_telegram(
-                        text=f'{location}\n{price}'
-                             f'\n{title}\n\n{format_href}',
-                        img=image,
-                        chat_id=chat_id,
-                    )
-                else:
+            for href_image in hrefs_and_images:
+                href, image, title, price, location = href_image
+                format_href = href.split('#')[0]
+
+                if format_href not in old_hrefs:
                     logging.info(
-                        f'[olx.ua] Знайдено стоп слово "{stop}" '
-                        f'у назві "{title}"'
+                        f'[olx.ua] Знайдено нове оголошення: {format_href}'
                     )
+                    ad_found = True
+                    stop = False
+                    sent_status = 'false'
+                    for stop_word in STOP_WORDS:
+                        if stop_word.lower() in title.lower():
+                            stop = stop_word
+                            break
 
-                if stop or sent_status:
-                    write_data(href=format_href, chat_id=chat_id)
-                elif (
-                        sent_status.get('error_code') == 400 and
-                        sent_status.get('description') ==
-                        "Bad Request: wrong file identifier/HTTP URL specified"
-                ):
-                    logging.info(
-                        f'[olx.ua] Проблема з оголошенням {format_href} '
-                        f'({sent_status.get("description")})'
-                    )
-                    write_data(href=format_href, chat_id=chat_id)
-        if not ad_found:
-            logging.info('[olx.ua] Нових оголошень поки що немає')
-    else:
-        raise Exception(f'Треба перевірити локатори!\n{req}\n')
+                    if not stop:
+                        sent_status = send_telegram(
+                            text=f'{location}\n{price}'
+                                 f'\n{title}\n\nhttps://www.olx.ua'
+                                 f'{format_href}',
+                            img=image,
+                            chat_id=chat_id,
+                        )
+                    else:
+                        logging.info(
+                            f'[olx.ua] Знайдено стоп слово "{stop}" '
+                            f'у назві "{title}"'
+                        )
+
+                    if stop or sent_status:
+                        write_data(href=format_href, chat_id=chat_id)
+                    elif (
+                            sent_status.get('error_code') == 400 and
+                            sent_status.get('description') ==
+                            "Bad Request: wrong file identifier/HTTP URL "
+                            "specified"
+                    ):
+                        logging.info(
+                            f'[olx.ua] Проблема з оголошенням {format_href} '
+                            f'({sent_status.get("description")})'
+                        )
+                        write_data(href=format_href, chat_id=chat_id)
+            if not ad_found:
+                logging.info('[olx.ua] Нових оголошень поки що немає')
+            olx_work = True
+            break
+        # else:
+        #     raise Exception(
+        #         f'Треба перевірити локатори!\n{requests_url}\n'
+        #     )
+    if not olx_work:
+        raise Exception(
+            f'Треба перевірити локатори!\n{requests_url}\n'
+        )
 
 
 def run(city=None, hata=None, chat_id=None):
